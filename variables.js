b7eea89ecd5c4ec4dad77b49385ae9c119ed93d9
Fernando Zalazar <f.zalazar@live.com>
2022-04-21 11:04:50 -0300

    variables para imprimir en pantalla y consola

---
+++ variables.js
@@ -0,0 +1,44 @@
+//parte 1
+var intUno = 142;
+console.log( intUno );
+
+var intDos = 284;
+console.log( intDos );
+
+var floatUno= 3.14;
+console.log( floatUno );
+
+var floatDos = 0.75;
+console.log( floatDos );
+
+var stringUno = "grande de fugazzeta";
+console.log( stringUno );
+
+var stringDos = "cafe";
+console.log( stringDos );
+ 
+var booleanUno = false;
+console.log( booleanUno );
+
+var booleanDos = true;
+console.log( booleanDos );
+//parte 1 finalizada crear variables e imprimilas
+
+
+//parte2
+
+document.write(`
+
+<h2> entero uno: ${ intUno } </h2>
+<h2> entero dos: ${ intDos } </h2>
+<h2> float uno: ${ floatUno } </h2>
+<h2> float dos: ${ floatDos } </h2>
+<h2> string uno: ${ stringUno } </h2>
+<h2> string dos: ${ stringDos } </h2>
+<h2> boolean uno: ${ booleanUno } </h2>
+<h2> boolean dos: ${ booleanDos } </h2>
+
+
+
+
+`)
